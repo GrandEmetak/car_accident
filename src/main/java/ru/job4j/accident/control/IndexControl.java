24b7e8046f75e6f3ef52ed6d507de3127c166d59
SlartiBartFast-art <JekHamond@tutanota.com>
2021-12-03 18:02:37 +0500

    1. Загрузка данных в вид [#295782 #235528] Уровень : 3. МидлКатегория : 3.4. SpringТопик : 3.4.2. MVC

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 0. Spring MVC [#6877]
  * Уровень : 3. МидлКатегория : 3.4. SpringТопик : 3.4.2. MVC
@@ -11,11 +14,26 @@ import org.springframework.web.bind.annotation.GetMapping;
  * Spring сканирует проект и регистрирует этот контроллер. Когда на Dispacher приходит запрос, он ищет подходящий контроллер.
  * <p>
  * Метод index возвращает имя вида без расширения.
+ * 1. Загрузка данных в вид [#295782]
+ * Уровень : 3. МидлКатегория : 3.4. SpringТопик : 3.4.2. MVC
+ * мы передадим данные из контроллера в вид.
+ * Метод index принимает объект Model. Этот тот же HttpServletRequest,
+ * в который можно добавить параметры.
+ *  model.addAttribute("user", "Petr Arsentev"); -  получать в jsp по атрибуьу user  ${user}
+ * -. Добавьте в проект bootstap и выведите на нем таблицу List<String> через jstl.
  */
 @Controller
 public class IndexControl {
+
     @GetMapping("/")
     public String index(Model model) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Petr Arsentev");
+        stringList.add("Sam Brown");
+        stringList.add("John Smith");
+
+        model.addAttribute("user", "Petr Arsentev");
+        model.addAttribute("clients", stringList);
         return "index";
     }
 }
